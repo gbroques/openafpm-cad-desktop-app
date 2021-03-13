20d6b27c6ba2f7c668a25bd9a195b9688f4e5102
G Roques <groques360@gmail.com>
2021-03-12 19:30:44 -0600

    Add semi-colons

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 const { app, BrowserWindow } = require('electron');
-
 const server = require('./server');
 
-server.listen(3000, () =>
-  console.log('Visit http://127.0.0.1:3000')
-);
+server.listen(3000, () => console.log('Visit http://127.0.0.1:3000'));
 
 function createWindow () {
-  const win = new BrowserWindow({
+  const window = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: false
     }
-  })
+  });
 
-  win.loadURL('http://127.0.0.1:3000')
+  window.loadURL('http://127.0.0.1:3000');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
