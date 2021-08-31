406d0b3b971c8def108118add551418ec1db1c6b
G Roques <groques360@gmail.com>
2021-08-31 18:44:32 -0500

    Fix get_default_parameters

--- src/get_default_parameters.py
+++ src/get_default_parameters.py
@@ -9,20 +9,12 @@ import sys
 
 sys.path.append(os.environ['FREECAD_LIB'])
 import json
-from enum import Enum, unique
 
 import FreeCAD
-from openafpm_cad_core.app import get_default_parameters
-
-
-@unique
-class WindTurbine(Enum):
-    T_SHAPE = 'T Shape'
-    H_SHAPE = 'H Shape'
-    STAR_SHAPE = 'Star Shape'
+from openafpm_cad_core.app import WindTurbine, get_default_parameters
 
 print(json.dumps({
-    WindTurbine.T_SHAPE.value: get_default_parameters(WindTurbine.T_SHAPE.value),
-    WindTurbine.H_SHAPE.value: get_default_parameters(WindTurbine.H_SHAPE.value),
-    WindTurbine.STAR_SHAPE.value: get_default_parameters(WindTurbine.STAR_SHAPE.value)
+    WindTurbine.T_SHAPE.value: get_default_parameters(WindTurbine.T_SHAPE),
+    WindTurbine.H_SHAPE.value: get_default_parameters(WindTurbine.H_SHAPE),
+    WindTurbine.STAR_SHAPE.value: get_default_parameters(WindTurbine.STAR_SHAPE)
 }, indent=2))
