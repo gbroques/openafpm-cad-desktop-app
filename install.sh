1f23bd500edc39118a7d5f84b2e0725abf67f4b4
G Roques <groques360@gmail.com>
2023-01-16 22:37:53 -0600

    Fix install.sh for Windows

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-# ---------------------------------------------
-# Install FreeCAD, openafpm-cad-core module,
-# and create .env file.
-#
-# Supports Linux and Windows operating systems.
-# ---------------------------------------------
+# ===============================================
+# Install FreeCAD, openafpm-cad-core module,    #
+# and create .env file.                         #
+#                                               #
+# Supports Linux and Windows operating systems. #
+# ===============================================
 
 # Exit on error. 
 set -e
@@ -27,11 +27,12 @@ is_linux()
   [ "$(uname --kernel-name)" == "Linux" ]
 }
 
-if [ is_linux ]; then
+if is_linux; then
   freecad_download_link='https://github.com/FreeCAD/FreeCAD/releases/download/0.20.2/FreeCAD_0.20.2-2022-12-27-conda-Linux-x86_64-py310.AppImage'
   archive='freecad.AppImage'
 else
-  freecad_download_link='https://github.com/FreeCAD/FreeCAD/releases/download/0.20.2/FreeCAD-0.20.2-WIN-x64-portable-2.zip'
+  # Use 0.20.1 instead of 0.20.2 because extracting portable 0.20.2 Windows zip errors on certain files.
+  freecad_download_link='https://github.com/FreeCAD/FreeCAD/releases/download/0.20.1/FreeCAD-0.20.1-WIN-x64-portable-1.zip'
   archive='freecad.zip'
 fi
 
@@ -42,7 +43,7 @@ else
   echo 'FreeCAD already downloaded.'
 fi
 
-if [ is_linux ]; then
+if is_linux; then
   chmod a+x $archive
   extracted_directory='squashfs-root'
   if [ ! -d "$extracted_directory" ]; then
@@ -53,9 +54,14 @@ if [ is_linux ]; then
   fi
 else
   extracted_directory='FreeCAD-portable'
-  unzip $archive -d $extracted_directory
+  if [ ! -d "$extracted_directory" ]; then
+    echo 'Extracting FreeCAD archive.'
+    validate_command_exists unzip
+    unzip $archive -d $extracted_directory
+  else
+    echo 'FreeCAD archive already extracted.'
+  fi
 fi
-
 
 # Install openafpm-cad-core module
 # --------------------------------
@@ -63,7 +69,7 @@ validate_command_exists git
 
 install_gbroques_module()
 {
-  if [ is_linux ]; then
+  if is_linux; then
     module_directory="$extracted_directory/usr/Mod/$1"
   else
     module_directory="$extracted_directory/Mod/$1"
@@ -81,6 +87,7 @@ install_gbroques_module freecad-to-obj
 
 get_path()
 {
+  validate_command_exists realpath
   realpath `find $extracted_directory -type f -name "$1"`
 }
 
@@ -88,12 +95,13 @@ get_path()
 # ----------------
 if [ ! -f '.env' ]; then
   echo "Creating .env file."
-  if [ is_linux ]; then
+  if is_linux; then
     python_path=$(get_path python)
     freecad_lib_directory=$(get_path FreeCAD.so | xargs -0 dirname)
   else
-    python_path=$(get_path python.exe)
-    freecad_lib_directory=$(get_path FreeCADApp.dll | xargs -0 dirname)
+    validate_command_exists cygpath
+    python_path=$(cygpath -w `realpath "$extracted_directory/bin/python.exe"`)
+    freecad_lib_directory=$(cygpath -w $(get_path FreeCADApp.dll | xargs -0 dirname))
   fi
   echo "PYTHON=$python_path" > .env
   echo "FREECAD_LIB=$freecad_lib_directory" >> .env
