e26c9857a24a78f1ca4fd79f5089595516edc808
G Roques <groques360@gmail.com>
2024-08-01 21:22:27 -0500

    Make 'files' singular in import button title

--- frontend/inputsForm.js
+++ frontend/inputsForm.js
@@ -211,7 +211,7 @@ class InputsForm extends LitElement {
           </label>
           <div>
             <input id="file-upload" type="file" accept="application/json,.mat" hidden @change=${this.handleFileUploadChange} />
-            <md-text-button class="importButton" @click=${this.handleImport} title="Import JSON or .mat files">
+            <md-text-button class="importButton" @click=${this.handleImport} title="Import JSON or .mat file">
               Import
               <!-- file_download material icon -->
               <svg slot="icon" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 9h-4V3H9v6H5l7 7 7-7zM5 18v2h14v-2H5z"/></svg>
