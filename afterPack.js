848ba6be8abba19b6042266d1ccfd7f1e9042bfb
G Roques <groques360@gmail.com>
2025-01-24 21:00:05 -0600

    Copy node_modules manually afterPack since electron-builder doesn't copy all of them

--- afterPack.js
+++ afterPack.js
@@ -1,10 +1,10 @@
 /**
  * electron-builder fails to copy squashfs-root directory
- * due to symlinks.
+ * due to symlinks, and failes to copy all of node_modules.
  *
  * To workaround this, we instruct electron-builder to NOT copy
- * the squashfs-root directory in the build configuration, and
- * manually copy it with the cp command after packing.
+ * the squashfs-root and node_modules directories in the build
+ * configuration, and manually copy it with the cp command after packing.
  */
 const path = require('path');
 const { exec } = require('child_process');
@@ -12,21 +12,24 @@ const { exec } = require('child_process');
 exports.default = async function(context) {
   const { appOutDir } = context;
   const appDir = path.join(appOutDir, 'resources', 'app');
-  const squashfsRootPath = path.join(__dirname, 'squashfs-root');
-  const copyCommand = `cp --recursive ${squashfsRootPath} ${appDir}`;
-  exec(copyCommand, (error, stdout, stderr) => {
-    if (error) {
-      if (stdout) {
-        console.log(stdout);
+  const directories = ['squashfs-root', 'node_modules'];
+  for (const directory of directories) {
+    const directoryPath = path.join(__dirname, directory);
+    const copyCommand = `cp --recursive ${directoryPath} ${appDir}`;
+    exec(copyCommand, (error, stdout, stderr) => {
+      if (error) {
+        if (stdout) {
+          console.log(stdout);
+        }
+        console.error(error);
+      } else {
+        if (stdout) {
+          console.log(stdout);
+        }
+        if (stderr) {
+          console.error(stderr);
+        }
       }
-      console.error(error);
-    } else {
-      if (stdout) {
-        console.log(stdout);
-      }
-      if (stderr) {
-        console.error(stderr);
-      }
-    }
-  });
+    });
+  }
 }
