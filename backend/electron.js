546de08d6644eb1ba5397c2622cdb8990bbb61b2
G Roques <groques360@gmail.com>
2025-02-23 17:21:27 -0600

    Add 'View' -> 'Print to PDF' file menu option

--- backend/electron.js
+++ backend/electron.js
@@ -1,5 +1,7 @@
-const { app: electronApp, BrowserWindow, nativeTheme, ipcMain } = require('electron');
+const { app: electronApp, BrowserWindow, nativeTheme, ipcMain, shell, Menu } = require('electron');
 const path = require('path');
+const os = require('node:os');
+const fs = require('node:fs');
 const http = require('http');
 const process = require('process');
 const { spawn } = require('child_process');
@@ -118,6 +120,7 @@ nativeTheme.themeSource = 'light';
 electronApp.whenReady()
   .then(createWindow)
   .then(async (window) => {
+    createMenu(window);
     window.loadFile('loading.html');
     const port = await portfinder.getPortPromise({ port: 8000, stopPort: 65535 });
     const pythonPath = path.join(rootPath, process.env.PYTHON);
@@ -145,3 +148,130 @@ electronApp.whenReady()
       childProcess.kill();
     });
   });
+
+
+function createMenu(window) {
+  const isMac = process.platform === 'darwin';
+
+  const template = [
+    // { role: 'appMenu' }
+    ...(isMac
+      ? [{
+          label: app.name,
+          submenu: [
+            { role: 'about' },
+            { type: 'separator' },
+            { role: 'services' },
+            { type: 'separator' },
+            { role: 'hide' },
+            { role: 'hideOthers' },
+            { role: 'unhide' },
+            { type: 'separator' },
+            { role: 'quit' }
+          ]
+        }]
+      : []),
+    // { role: 'fileMenu' }
+    {
+      label: 'File',
+      submenu: [
+        isMac ? { role: 'close' } : { role: 'quit' }
+      ]
+    },
+    // { role: 'editMenu' }
+    {
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        ...(isMac
+          ? [
+              { role: 'pasteAndMatchStyle' },
+              { role: 'delete' },
+              { role: 'selectAll' },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [
+                  { role: 'startSpeaking' },
+                  { role: 'stopSpeaking' }
+                ]
+              }
+            ]
+          : [
+              { role: 'delete' },
+              { type: 'separator' },
+              { role: 'selectAll' }
+            ])
+      ]
+    },
+    // { role: 'viewMenu' }
+    {
+      label: 'View',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forceReload' },
+        { role: 'toggleDevTools' },
+        { type: 'separator' },
+        { role: 'resetZoom' },
+        { role: 'zoomIn' },
+        { role: 'zoomOut' },
+        { type: 'separator' },
+        { role: 'togglefullscreen' },
+        {
+          label: 'Print to PDF',
+          click: () => {
+            const pdfPath = path.join(os.homedir(), 'Desktop', 'OpenAFPM CAD Desktop App.pdf');
+            const options = {printBackground: true, margins: {top: 0, right: 0, bottom: 0, left: 0}};
+            window.webContents.printToPDF(options).then(data => {
+              fs.writeFile(pdfPath, data, (error) => {
+                if (error) throw error;
+                shell.openExternal('file://' + pdfPath);
+              });
+            }).catch(error => {
+              console.error(`Failed to write PDF to ${pdfPath}: `, error);
+            });
+          }
+        }
+      ]
+    },
+    // { role: 'windowMenu' }
+    {
+      label: 'Window',
+      submenu: [
+        { role: 'minimize' },
+        { role: 'zoom' },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              { role: 'front' },
+              { type: 'separator' },
+              { role: 'window' }
+            ]
+          : [
+              { role: 'close' }
+            ])
+      ]
+    },
+    {
+      role: 'help',
+      submenu: [
+        {
+          label: 'Learn More',
+          click: async () => {
+            const { shell } = require('electron')
+            await shell.openExternal('https://electronjs.org')
+          }
+        }
+      ]
+    }
+  ]
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+}
+
