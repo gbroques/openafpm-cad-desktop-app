55500f4513f28a604cd68f3075ad3afc7a85d4cb
G Roques <groques360@gmail.com>
2025-11-18 02:34:39 -0600

    Add progress bar component

---
+++ frontend/progressBar.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from "lit";
+import "@material/web/progress/linear-progress.js";
+
+export default class ProgressBar extends LitElement {
+  static properties = {
+    progress: { type: Number },
+    message: { type: String }
+  };
+
+  static styles = css`
+    :host {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: calc(var(--spacing) * 2);
+      width: 100%;
+      max-width: 400px;
+    }
+
+    md-linear-progress {
+      width: 100%;
+    }
+
+    .progress-text {
+      font-size: var(--h6);
+      color: var(--text-color);
+      text-align: center;
+    }
+  `;
+
+  render() {
+    const progressValue = this.progress || 0;
+    const progressMessage = this.message || 'Loading...';
+    
+    return html`
+      <div class="progress-text">
+        ${progressMessage} - ${progressValue}%
+      </div>
+      <md-linear-progress 
+        .value="${progressValue}" 
+        max="100">
+      </md-linear-progress>
+    `;
+  }
+}
+
+customElements.define('x-progress-bar', ProgressBar);
